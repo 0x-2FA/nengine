a6fd236642245c937f3a07e82f748ebe6bdbf11a
0x-2FA <2fa_gh@protonmail.com>
2022-02-19 12:32:56 +0200

    Started implementing the create function

--- window.cpp
+++ window.cpp
@@ -1,8 +1,23 @@
 #include "window.h"
+#include "SDL2/SDL.h"
 
 namespace nengine
 {
   Window::Window(){}
   Window::~Window(){}
 
+  int Window::CreateWindow(char *name, int width, int height, unsigned int flags)
+  {
+    Uint32 fl = SDL_WINDOW_OPENGL;
+
+    if(flags & FULLSCREEN)
+    {
+      fl |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+    }
+
+    if(flags & BORDERLESS)
+    {
+      fl |= SDL_WINDOW_BORDERLESS;
+    }
+  }
 }
