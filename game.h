ba1686ef241ebc36504bcf3b2be48e8295f0c54a
0x-2FA <2fa_gh@protonmail.com>
2022-02-22 12:16:41 +0200

    Added the general game class (not ready yet)

---
+++ game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <window.h>
+
+namespace nengine
+{
+  class Game
+  {
+    public:
+     Game(){};
+     virtual ~Game(){};
+
+     void Run();
+
+     void CloseGame();
+
+     virtual void OnInit() = 0;
+     virtual void OnExit() = 0;
+
+     void OnEvent(SDL_Event &event);
+
+    private:
+
+  };
+
+}
+
+#endif // GAME_H
